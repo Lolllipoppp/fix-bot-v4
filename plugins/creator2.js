745de56c170cd5977c772bdd282303c5839f8848
Shpdie <114326041+Shpdie@users.noreply.github.com>
2022-10-24 00:21:55 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -6,7 +6,7 @@ let name = await conn.getName(who)
 
 
 if (command == 'whmods1') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Kenzo-Botz;Bot;;Md\nFN:Saya Owner Kenzo-Botz Bot WhatsApp, Md\nNICKNAME:👑 Owner Kenzo-Botz\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6281214557752:+62 812-1455-7752\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.instagram.com/Dmssrdn\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:Kepo wgwg\nitem3.X-ABLabel:💌 Mail Owner Kenzo-Botz\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Kenzo-Botz;Bot;;Md\nFN:Saya Owner Kenzo-Botz, Md\nNICKNAME:👑 Owner Kenzo-Botz\nORG:WH\nTITLE:MODS\nitem1.TEL;waid=6281214557752:+62 812-1455-7752\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://www.instagram.com/Dmssrdn\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:Kepo wgwg\nitem3.X-ABLabel:💌 Mail Owner Kenzo-Botz\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 KEPO LU YAH\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
